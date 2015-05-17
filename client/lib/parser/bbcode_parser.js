4b676853379a2e1369bf2653d467275b8160580f
jonhallur <jonhallur@gmail.com>
2015-05-17 23:16:04 +0000

    more parser fun.

--- client/lib/parser/bbcode_parser.js
+++ client/lib/parser/bbcode_parser.js
@@ -1,65 +1,71 @@
-TokenType = {
-	TEXT: 0,
-	WHITESPACE: 1,
-	FORMAT: 2
+var TokenType = {
+    TEXT: "text",
+    WHITESPACE: "whitespace",
+    FORMAT: "format"
 };
 
-WhiteSpaceType = {
-	SPACE: '\ ',
-	NEWLINE: '\n'
+var WhiteSpaceType = {
+    SPACE: '\ ',
+    NEWLINE: '\n'
 };
 
-FormatTag = {
-	OPEN: '[',
-	CLOSE: ']'
+var FormatTag = {
+    OPEN: '[',
+    CLOSE: ']',
+    END: '/'
 };
 
-FormatMethod = {
-	START: 0,
-	END: 1
+var FormatMethod = {
+    START: "start",
+    END: "end"
 };
 
-FormatType = {
-	STYLE: 0,
-	ALIGNMENT: 1,
-	SIZE: 2,
-	COLOR: 3
+var FormatType = {
+    STYLE: "style",
+    ALIGNMENT: "alignment",
+    SIZE: "size",
+    COLOR: "color"
 };
 
-FormatStyle = {
-	BOLD: "b",
-	ITALIC: "i",
-	UNDERLINE: "u"
+var FormatStyle = {
+    BOLD: "b",
+    ITALIC: "i",
+    UNDERLINE: "u"
 };
 
-FormatAligment = {
-	LEFT: "left",
-	CENTER: "center",
-	RIGHT: "right",
-	JUSTIFY: "justify"
+var FormatAligment = {
+    LEFT: "left",
+    CENTER: "center",
+    RIGHT: "right",
+    JUSTIFY: "justify"
 };
+
+var styleList = [FormatStyle.BOLD, FormatStyle.ITALIC, FormatStyle.UNDERLINE];
+var alignmentList = [FormatAligment.LEFT, FormatAligment.CENTER, FormatAligment.RIGHT, FormatAligment.JUSTIFY];
 
 function Token(type) {
-	this.tokenType = type;
+    "use strict";
+    this.tokenType = type;
 }
 
 function TextToken(text) {
-	Token.call(this, TokenType.TEXT);
-	this.textValue = text;
+    "use strict";
+    Token.call(this, TokenType.TEXT);
+    this.textValue = text;
 }
 
 function WhiteSpaceToken(whiteSpaceType) {
-	Token.call(this, TokenType.WHITESPACE);
-	this.whiteSpaceType = whiteSpaceType;
+    "use strict";
+    Token.call(this, TokenType.WHITESPACE);
+    this.whiteSpaceType = whiteSpaceType;
 }
 
 function FormatToken(formatMethod, formatType, value) {
-	Token.call(this, TokenType.FORMAT);
-	this.formatMethod = formatMethod;
-	this.formatType = formatType;
-	if(formatType == FormatType.SIZE || formatType == formatType.COLOR) {
-		this.value = value;
-	}
+    "use strict";
+    Token.call(this, TokenType.FORMAT);
+    this.formatMethod = formatMethod;
+    this.formatType = formatType;
+    this.value = value;
 }
 
 TextToken.prototype = Object.create(Token.prototype);
@@ -67,57 +73,98 @@ WhiteSpaceToken.prototype = Object.create(Token.prototype);
 FormatToken.prototype = Object.create(Token.prototype);
 
 
-BBCodeParser = function (text) {
-	this.parserLocation = 0;
-	this.text = text;
-	this.hasNext = true;
+var BBCodeParser = function (text) {
+    "use strict";
+    this.parserLocation = 0;
+    this.text = text;
+    this.hasNext = true;
 };
 
 BBCodeParser.prototype.getNextToken = function () {
-	if (!this.hasNext) {
-		return null;
-	}
-	var start = this.parserLocation;
-	var location = start;
-	var c = this.text[location];
-	if(c == WhiteSpaceType.NEWLINE || c == WhiteSpaceType.SPACE) {
-		this.parserLocation++;
-		return new WhiteSpaceToken(c);
-	}
-	if(c == FormatTag.OPEN) {
-		var next = this.text[location+1];
-		if (next == FormatStyle.BOLD || next == FormatStyle.ITALIC || next == FormatStyle.UNDERLINE) {
-			if (this.text[location+2] == FormatTag.CLOSE) {
-				this.parserLocation += 3;
-				return new FormatToken(FormatMethod.START, next, null);
-			}
-		}
-	}
-	for (; location < this.text.length; location++) {
-		if (this.text[location] === " ") {
-			this.parserLocation = location;
-			var word = this.text.slice(start, location);
-			return new TextToken(word);
-		}
-		else if (location == this.text.length-1) {
-			this.parserLocation = location;
-			var word = this.text.slice(start, location+1);
-			this.hasNext = false;
-			return new TextToken(word);
-		}
-	}
-};
+    "use strict";
+    if (!this.hasNext) {
+        return null;
+    }
+    var start = this.parserLocation, location = start, c = this.text[location], formatToken = null;
+    if (c === WhiteSpaceType.NEWLINE || c === WhiteSpaceType.SPACE) {
+        this.parserLocation++;
+        return new WhiteSpaceToken(c);
+    }
+    this._parseStyleFormat = function () {
+        var formatMethod = FormatMethod.START,
+            formatLength = 3,
+            styleCharLocation = start + 1;
+        if (styleCharLocation === this.text.length) {
+            return null;
+        }
+        if (this.text[styleCharLocation] === FormatTag.END) {
+            if (styleCharLocation + 1 === this.text.length) {
+                return null;
+            }
+            styleCharLocation++;
+            formatLength++;
+            formatMethod = FormatMethod.END;
 
-/*
-BBCodeParser.prototype.getNextWord = function () {
-	var start = this.parserLocation;
-	var location = start;
-	for (; location < this.text.length; location++) {
-		if (this.text[location] === " ") {
-			break;
-		}
-	}
-	this.parserLocation = location+1;
-	return this.text.slice(start, location)
+        }
+        if (styleList.indexOf(this.text[styleCharLocation]) !== -1) {
+            if (styleCharLocation + 1 === this.text.length) {
+                return null;
+            }
+            if (this.text[(styleCharLocation + 1)] === FormatTag.CLOSE) {
+                this.parserLocation = this.parserLocation + formatLength;
+                return new FormatToken(formatMethod, FormatType.STYLE, this.text[styleCharLocation]);
+            }
+        }
+        return null;
+    };
+
+    this._parseAlignmentFormat = function () {
+        var formatMethod = FormatMethod.START,
+            formatLengthModifier = 1,
+            styleCharLocation = start + 1;
+        if (styleCharLocation === this.text.length) {
+            return null;
+        }
+        if (this.text[styleCharLocation] === FormatTag.END) {
+            if (styleCharLocation + 1 === this.text.length) {
+                return null;
+            }
+            styleCharLocation++;
+            formatLengthModifier++;
+            formatMethod = FormatMethod.END;
+        }
+        for (var index in alignmentList) {
+            var tagLength = alignmentList[index].length;
+            if (this.text.length < styleCharLocation + tagLength) {
+                return null;
+            } else if (this.text.slice(styleCharLocation, styleCharLocation + tagLength) === alignmentList[index]){
+                return new FormatToken(FormatMethod.START, FormatType.ALIGNMENT, alignmentList[index]);
+            }
+
+        }
+
+        return null;
+    };
+    if (c === FormatTag.OPEN) {
+        formatToken = this._parseStyleFormat();
+        if (formatToken !== null) {
+            return formatToken;
+        }
+        formatToken = this._parseAlignmentFormat();
+        if (formatToken !== null) {
+            return formatToken;
+        }
+    }
+
+    for (location; this.text.length > location; location++) {
+        if (this.text[location] === " ") {
+            this.parserLocation = location;
+            return new TextToken(this.text.slice(start, location));
+        }
+        if (location === this.text.length - 1) {
+            this.parserLocation = location;
+            this.hasNext = false;
+            return new TextToken(this.text.slice(start, location + 1));
+        }
+    }
 };
-*/
--- test/mySpec.js
+++ test/mySpec.js
@@ -41,5 +41,77 @@ describe("BBCode parser", function(){
         expect(secondToken.tokenType).toBe(TokenType.WHITESPACE);
         expect(thirdToken.tokenType).toBe(TokenType.TEXT);
         expect(thirdToken.textValue).toBe("space");
-    })
+    });
+
+    it("should ignore an ending format tag", function() {
+        "use strict";
+        var text = "[";
+        var parser = new BBCodeParser(text);
+        var firstToken = parser.getNextToken();
+        expect(firstToken.tokenType).toBe(TokenType.TEXT);
+        expect(firstToken.textValue).toBe(text);
+    });
+
+    it("should ignore and unfinished short style tag", function() {
+        "use strict";
+        var text = "[b";
+        var parser = new BBCodeParser(text);
+        var firstToken = parser.getNextToken();
+        expect(firstToken.tokenType).toBe(TokenType.TEXT);
+        expect(firstToken.textValue).toBe(text);
+    });
+
+    it("should return a valid [b] token", function () {
+        "use strict";
+        var text = "[b]";
+        var parser = new BBCodeParser(text);
+        var firstToken = parser.getNextToken();
+        expect(firstToken.tokenType).toBe(TokenType.FORMAT);
+        expect(firstToken.formatMethod).toBe(FormatMethod.START);
+        expect(firstToken.value).toBe(FormatStyle.BOLD);
+    });
+
+    it("should return text on wrongly formatted bold tag", function () {
+        "use strict";
+        var text = "[bold]";
+        var parser = new BBCodeParser(text);
+        var firstToken = parser.getNextToken();
+        expect(firstToken.tokenType).toBe(TokenType.TEXT);
+    });
+
+    it("should return a valid [/b] token", function () {
+        "use strict";
+        var text = "[/b]";
+        var parser = new BBCodeParser(text);
+        var firstToken = parser.getNextToken();
+        expect(firstToken.tokenType).toBe(TokenType.FORMAT);
+        expect(firstToken.formatMethod).toBe(FormatMethod.END);
+        expect(firstToken.value).toBe(FormatStyle.BOLD);
+    });
+
+    it("should return a close format tag next to text next to space", function () {
+        "use strict";
+        var text = "[/b]space ";
+        var parser = new BBCodeParser(text);
+        var firstToken = parser.getNextToken();
+        var secondToken = parser.getNextToken();
+        var thirdToken = parser.getNextToken();
+        expect(firstToken.tokenType).toBe(TokenType.FORMAT);
+        expect(firstToken.formatMethod).toBe(FormatMethod.END);
+        expect(firstToken.value).toBe("b");
+        expect(secondToken.tokenType).toBe(TokenType.TEXT);
+        expect(secondToken.textValue).toBe("space");
+        expect(thirdToken.tokenType).toBe(TokenType.WHITESPACE);
+    });
+
+    it("should return a valid [left] token", function () {
+        "use strict";
+        var text = "[left]";
+        var parser = new BBCodeParser(text);
+        var firstToken = parser.getNextToken();
+        expect(firstToken.tokenType).toBe(TokenType.FORMAT);
+        expect(firstToken.formatMethod).toBe(FormatMethod.START);
+        expect(firstToken.value).toBe(FormatAligment.LEFT);
+    });
+
 });
